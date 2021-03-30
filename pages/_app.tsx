ac18dc18562d203c3ca70df229f106b88e8bb286
Gleeming <33912546+GleemingKnight@users.noreply.github.com>
2021-03-29 22:37:10 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,8 @@ import { AppProps } from 'next/app'
 
 import '../styles/index.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default App
