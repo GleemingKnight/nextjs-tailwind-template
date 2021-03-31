c5ae9f9b6bc131fec83c8ce87182f44573017d2e
gleemingknight <7jashton@gmail.com>
2021-03-31 16:20:53 -0400

    Fixed an error

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
+    req.cookies
     res.status(200).json({"API": "Next.js API"})
 }
 
